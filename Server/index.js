a5c107ba3e86f3fa8b93f8e0596eee6e461ed36c
Deepali Garg <hdgarg@gmail.com>
2018-07-18 22:13:50 -0700

    webpack, server, mysql running corectly

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/MainFeed', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World. I am the server')
 })
 
 console.log('listening on port 3000!')
