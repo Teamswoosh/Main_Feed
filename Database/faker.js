65427de538e70a3be8d0a92bc64dfe73e668cc88
Deepali Garg <hdgarg@gmail.com>
2018-07-20 20:03:50 -0700

    installs eslint

--- Database/faker.js
+++ Database/faker.js
@@ -1,37 +1,35 @@
 const MAX_NUMBER_OF_SHOES = 100;
 
-const connection = require('./index');
 const faker = require('faker');
+const connection = require('./index');
 
 
 // const mysql_faker = require('mysql-faker');
 
-//function to generate a random number 0 or 1 corresponding to 'no' or 'yes'
+// function to generate a random number 0 or 1 corresponding to 'no' or 'yes'
 const randomInStockGenerator = () => {
-  let array = [];
-  
-  for (let i = 0; i < MAX_NUMBER_OF_SHOES ; i++) {
+  const array = [];
+
+  for (let i = 0; i < MAX_NUMBER_OF_SHOES; i++) {
     const number = faker.random.number({ min: 0, max: 1 });
-    array.push('(' + number + ')');
+    array.push(`(${number})`);
   }
   return array.join();
 };
 // console.log(randomInStockGenerator());
 
-//function to populate the inStockOptions table with random values for the field inStockOption
-const inStockOption = (cb) => {  
-  connection.query('INSERT INTO inStockOptions (inStock) VALUES' + randomInStockGenerator(), function(error) {
+// function to populate the inStockOptions table with random values for the field inStockOption
+const inStockOption = () => {
+  connection.query(`INSERT INTO inStockOptions (inStock) VALUES${randomInStockGenerator()}`, (error) => {
     if (error) throw error;
     console.log('random values of 0 or 1 have been added to the inStock field');
   });
-}
+};
 
 inStockOption();
 
 module.exports = {
-  inStockOption
+  inStockOption,
 };
 
 connection.end();
-
-
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,7 @@
 
 // https://www.nike.com/t/air-jordan-1-retro-high-og-shoe-WR35rK/555088-401
 // deliverables tracker: https://docs.google.com/spreadsheets/d/1yP2GGG4pzrZwWRA3qO9RRbWKM0QNZh-8L-SVkCPShnU/edit?usp=sharing
+//https://drive.google.com/drive/folders/1NNJxzLHb3WsMkMwXl1X0gqOivyxZrHTd
 import React from "react";
 import style from "./styles/style.css";
 
