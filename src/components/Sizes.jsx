f7069d85dcb99283348138b14c4e901bdac4cb8d
Deepali Garg <hdgarg@gmail.com>
2018-07-23 23:20:01 -0700

    adds Choose size and size guide as a flex component

--- src/components/Sizes.jsx
+++ src/components/Sizes.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import style from '../styles/style.css';
 
 const Sizes = () => (
   <div>
+    <div className={style.chooseSizeAndGuide}> 
+      <h6 className={style.chooseSize}>CHOOSE SIZE</h6>
+      <h6 className={style.sizeGuide}> Size Guide</h6>
+    </div>
       
   </div>
 );
--- src/components/Thumbs.jsx
+++ src/components/Thumbs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from "../styles/style.css";
 
+
 const Thumbs = () => (
   <div>
     <img src='https://s3-us-west-1.amazonaws.com/fecmainfeed/Main_feed_ims/94.jpeg' height='50' width='50' />
